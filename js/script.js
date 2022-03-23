e084845a6124c1cc32fd48a996242992cfd1a693
Beniamin Guras <gurasbeniamin@gmail.com>
2022-03-23 20:39:47 +0100

    Add article active

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 'use strict';
 
 function titleClickHandler(event) {
+    const clickedElement = this;
     console.log(event);
     /* remove class 'active' from all article links  */
-    links.document
+    const activeLinks = document.querySelectorAll(".titles a.active");
+    
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+        console.log('Klasa acrive usunieta')
+    }
+
     /* add class 'active' to the clicked link */
-
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+    
     /* remove class 'active' from all articles */
-
+    const activeArticles = document.querySelectorAll('.post');
+    for(let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
